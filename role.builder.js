d00e3bae6cb487a9012f69ed3beddb2358cd2d29
Victor Krogh Hansen <kingodk@outlook.dk>
2022-08-10 15:04:39 +0200

    Removed comment

--- role.builder.js
+++ role.builder.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.builder');
- * mod.thing == 'a thing'; // true
- */
-
 const { shouldCreepWork } = require('functions');
 const roleRepair = require('role.repair');
 
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,11 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.upgrader');
- * mod.thing == 'a thing'; // true
- */
 const { shouldCreepWork } = require('functions');
 
 var roleUpgrader = {
